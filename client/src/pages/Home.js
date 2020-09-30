9028bf6aa5e93f6cb78a027603c168c426b10b33
Cyrus Jose <cyrus.jose@outlook.com>
2020-09-30 16:41:39 -0700

    Made navbar with proper links

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
-
+import Navbar from "../components/Navbar/Navbar";
 function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Home</h1>
       <p>
         Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
