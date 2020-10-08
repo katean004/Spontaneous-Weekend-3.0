d24e683db3e61b5c58d44c5708f095174a226e53
Melinda Winter <melindawinter42@gmail.com>
2020-10-08 14:59:43 -0700

    fix signup forms

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,44 +4,41 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default class Login extends Component {
   render() {
     return (
-      <form>
-        <h3>Log In</h3>
-
-        <div className="form-group">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-          />
-        </div>
-
-        <div className="form-group">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-          />
-        </div>
-
-        <div className="form-group">
-          <div className="custom-control custom-checkbox">
-            <input
-              type="checkbox"
-              className="custom-control-input"
-              id="customCheck1"
-            />
-            <label className="custom-control-label" htmlFor="customCheck1">
-              Remember me
-            </label>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-md-offset-3">
+            <h2 class="loginHead">Welcome Back!</h2>
+            <h3 class="loginHead">Log in to get some weekend ideas.</h3>
+            <form class="login">
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="email-input"
+                  placeholder="Email"
+                />
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="password-input"
+                  placeholder="Password"
+                />
+              </div>
+              <button type="submit" class="btn btn-default">
+                Login
+              </button>
+            </form>
+            <br />
+            <p>
+              Don't have an account? Sign up <a href="/signup">here</a>.
+            </p>
           </div>
         </div>
-
-        <button type="submit" className="btn btn-primary btn-block">
-          Submit
-        </button>
-      </form>
+      </div>
     );
   }
 }
