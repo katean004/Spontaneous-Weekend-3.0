6b78cbfbd42bb99d826b1ada97e5754b88cedc22
Melinda Winter <melindawinter42@gmail.com>
2020-10-09 17:34:59 -0700

    set up for simpler login

---
+++ backend/controllers/movie.js
@@ -0,0 +1,6 @@
+// findAllUserMovies: function(req, res) {
+//     db.Movie
+//       .find({ userID: req.params.id })
+//       .then(dbModel => res.json(dbModel))
+//       .catch(err => res.status(422).json(err));
+//   },
