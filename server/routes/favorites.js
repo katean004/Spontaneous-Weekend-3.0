b036c4e94eb45ef641be67a82ec156fc750afc27
Cyrus Jose <cyrus.jose@outlook.com>
2020-10-10 21:56:23 -0700

    added routes

---
+++ server/routes/favorites.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("hellowlkshjaf;lkjsa;ldfj");
+});
+
+router.post("/", (req, res) => {
+ /*
+ ============== Task ==============
+
+    Figure out how to post to the favorites page.
+ */
+});
+
+module.exports = router;
