17fa477c12fcaa052f07c9dd16132b3ff94ae05e
Cyrus Jose <cyrus.jose@outlook.com>
2020-10-11 11:24:20 -0700

    making routes

--- server/routes/movie.js
+++ server/routes/movie.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const FavoriteMovie = require("../database/models/movie");
 
 /*
 ============ Task ============
 
 Find a way to get movie from the component and post it into the favorites page
 */
-router.get("/", (req, res) => {
-  res.send("hello world");
+router.get("/", async (req, res) => {
+  try {
+    const favorite__movie = await FavoriteMovie.find();
+    res.json(favorite__movie);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
+
+router.post("/", async(req,res)=>{
+  const favorite__movie = await FavoriteMovie
+})
 
 module.exports = router;
