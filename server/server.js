2b62513d8cf77c7b4d6e5a9bd36e05eb9a4b6431
Cyrus Jose <cyrus.jose@outlook.com>
2020-10-15 15:15:57 -0700

    Changed favorites to specify movies

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = 8080;
 // Route requires
 const user = require("./routes/user");
-const favoritesRouter = require("./routes/favorites");
+const favoriteMoviesRouter = require("./routes/favoriteMovies");
 
 // MIDDLEWARE
 app.use(morgan("dev"));
@@ -41,7 +41,7 @@ app.use(passport.session()); // calls the deserializeUser
 
 // Routes
 app.use("/user", user);
-app.use("/favorites", favoritesRouter);
+app.use("/favoriteMovies", favoriteMoviesRouter);
 
 // Starting Server
 app.listen(PORT, () => {
