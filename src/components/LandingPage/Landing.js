2c3e28073bfaddbb012e0c5ad741c0f20cc21d86
Cyrus Jose <cyrus.jose@outlook.com>
2020-10-15 18:24:20 -0700

    Testing media queries

--- src/components/LandingPage/Landing.js
+++ src/components/LandingPage/Landing.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import "./Landing.css"
+import React from "react";
+import "./Landing.css";
 
 export default function Landing() {
-    return (
-        <div className="hero">
-            
+  return (
+    <div className="container-fluid">
+      <div className="maincard">
+        <div className="mainheader">
+          <h1> Hello and welcome!</h1>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
