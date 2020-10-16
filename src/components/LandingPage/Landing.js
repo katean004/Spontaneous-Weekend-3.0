01769faf1995c9e58aa6f2370d46aa07f4f3c41c
Cyrus Jose <cyrus.jose@outlook.com>
2020-10-15 18:42:49 -0700

    Fixed redirect and home page

--- src/components/LandingPage/Landing.js
+++ src/components/LandingPage/Landing.js
@@ -3,10 +3,10 @@ import "./Landing.css";
 
 export default function Landing() {
   return (
-    <div className="container-fluid">
+    <div className="container-fluid landing__container">
       <div className="maincard">
         <div className="mainheader">
-          <h1> Hello and welcome!</h1>
+          <h1 className="landingHeader"> Hello and welcome!</h1>
         </div>
       </div>
     </div>
