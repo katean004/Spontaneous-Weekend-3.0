d1c45d5c9ff1d6a667592dd193002fd3101bb002
Cyrus Jose <cyrus.jose@outlook.com>
2020-10-15 21:50:07 -0700

    Adding delete tomorrow

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,29 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Favorite.css";
-import Fade from "../Fade/Fade"
-import {v4 as uuidv4} from "uuid";
+import Fade from "../Fade/Fade";
+import { v4 as uuidv4 } from "uuid";
 
 function Favorites(props) {
   // ============= Test to see what as in here =============
   console.log(props.databaseInfo);
 
+  const [favoriteMovie, setFavoriteMovie] = useState(props.databaseInfo);
+
+  function handleMovieDelete(evt, id) {
+    evt.preventDefault();
+    console.log("============ ID of movie clicked ============");
+    console.log(id);
+    // Fetch with the method delete
+    // Put this filter in the router.
+    setFavoriteMovie(
+      favoriteMovie.filter(movie => {
+        // console.log(movie);
+        // console.log(movie._id)
+        movie._id !== id;
+      })
+    );
+  }
+
   return (
     <div>
       <h1 className="favorite__header">Favorites</h1>
-      <button className = "favorites__refresh"onClick={()=> window.location.reload(true)}>Refresh Favorites</button>
+      <button
+        className="favorites__refresh"
+        onClick={() => window.location.reload(true)}
+      >
+        Refresh Favorites
+      </button>
       {props.databaseInfo.length || props.foodDatabase.length ? (
         <div className="container-fluid">
           <div className=" movie__row">
             <h1 className="favorite__header">Movies</h1>
             {props.databaseInfo.map(movie => (
               // Needed for differentiation
-              <Fade key = {uuidv4()}>
-              <div className="movie__SubRow fade-in2" key={movie._id}>
-                <button>Delete</button>
-                <div className="movie__title">{movie.title}</div>
-                <div className="movie__rating">Rating: {movie.rating}</div>
-                <div className="movie__description">{movie.description}</div>
-              </div>
+              <Fade key={uuidv4()}>
+                <div className="movie__SubRow fade-in2" key={movie._id}>
+                  <button onClick={(evt) => handleMovieDelete(evt, movie._id)}>
+                    Delete
+                  </button>
+                  <div className="movie__title">{movie.title}</div>
+                  <div className="movie__rating">Rating: {movie.rating}</div>
+                  <div className="movie__description">{movie.description}</div>
+                </div>
               </Fade>
             ))}
           </div>
@@ -33,28 +57,26 @@ function Favorites(props) {
             {props.foodDatabase.map(food => (
               // Needed for differentiation
               <Fade key={uuidv4()}>
-              <div className="restaurant__SubRow fade-in2" key={food._id}>
-                <button>Delete</button>
-                <div className="movie__title">{food.name}</div>
-                <div className="movie__rating">Address: {food.address}</div>
-              </div>
+                <div className="restaurant__SubRow fade-in2" key={food._id}>
+                  <button>Delete</button>
+                  <div className="movie__title">{food.name}</div>
+                  <div className="movie__rating">Address: {food.address}</div>
+                </div>
               </Fade>
             ))}
           </div>
-
         </div>
       ) : (
         <div className="favorites__placholder__container">
           <div className="favorites__placeholder">
-            <p className="placeholder__paragraph">Hello! It seems like you don't have any favorites yet!</p>
+            <p className="placeholder__paragraph">
+              Hello! It seems like you don't have any favorites yet!
+            </p>
           </div>
         </div>
       )}
-      {/* The for loop/ for each will be inside here. Use a component here.*/}
     </div>
   );
 }
-
-// ================== Working on this part =================
 
 export default Favorites;
