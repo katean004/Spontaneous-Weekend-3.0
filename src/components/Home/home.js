d4bfc3018a0e898f76e2b48b7fc65faeef0470ef
Cyrus Jose <cyrus.jose@outlook.com>
2020-10-17 11:57:54 -0700

    finished Home page

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import "./home.css"
+import "./home.css";
+import Fade from "../Fade/Fade";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
   // <-------- Useless constructor. Not needed for now -------->
@@ -8,21 +10,40 @@ class Home extends Component {
   // }
 
   render() {
-    return(
+    return (
       <div className="homePage__Main">
         <div className="movieSection">
-          <div className="section__card">
-            <div className="section__content">
-            <h1>Movie Randomizer</h1>
-            <p>hello there</p>
-            </div>
-          </div>
+          <Fade>
+            <Link to="/movie" className="section__link">
+              <div className="section__card fade-in2">
+                <div className="section__content fade-in2">
+                  <h1>Movie Randomizer</h1>
+                  <p>
+                    Don't wanna think too hard when picking a movie
+                    <br />
+                    Let us do it for you! <br /> <br />
+                    Click on this card to head over to the movie page!
+                  </p>
+                </div>
+              </div>
+            </Link>
+          </Fade>
         </div>
         <div className="restaurantSection">
-          <div className="section__card">
-            <h1>Restaurant Randomizer</h1>
-            <p>hello there</p>
-          </div>
+          <Fade>
+            <Link to="/restaurant" className="section__link"></Link>
+            <div className="section__card">
+              <div className="section__content">
+                <h1>Restaurant Randomizer</h1>
+                <p>We often ask "What do you wanna eat?"
+                  <br/>
+                  Well, how about letting us do that for you too?
+                  <br/>
+                  Click on this card to head over to the restaurants page!
+                </p>
+              </div>
+            </div>
+          </Fade>
         </div>
       </div>
     );
