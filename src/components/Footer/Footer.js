5f7646d2fedcd7dda9f515dad2d5b5ec2367fda4
Cyrus Jose <cyrus.jose@outlook.com>
2020-10-17 14:28:30 -0700

    Fixed footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./footer.css";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <div>
       <footer className="mt-auto py-3 fixed-bottom">
+        <div className="container pb-2">
+          <span className="text-muted">Our Spontaneous Crew</span>
+        </div>
         <div className="container">
           <Link to="//github.com/cyrusjose">
             <span className="text-muted px-5">
@@ -22,7 +25,6 @@ export default function Footer() {
               <i className="fab fa-github contactIcon"> Kate An</i>
             </span>
           </Link>
-          
         </div>
       </footer>
     </div>
