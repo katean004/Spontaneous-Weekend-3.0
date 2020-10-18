8114dd29a87ec7de5d0dd563ef73bd6f041507aa
Melinda Winter <melindawinter42@gmail.com>
2020-10-17 22:55:49 -0700

    updates to favorites

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -8,47 +8,69 @@ function Favorites(props) {
   console.log(props.databaseInfo);
 
   return (
-    <div className="mainContainer">
-      <h1 className="favorite__header">Check out some crowd favorites</h1>
+    <div className="mainContainer container-fluid">
+      <div className="col-sm-12 col-m-6 col-lg-6 ml-auto mr-auto">
+        <h1 className="favorite__header">
+          Check out some crowd favorite movies <i className="fas fa-film"></i>{" "}
+          and restaurants <i className="fas fa-utensils"></i>{" "}
+        </h1>
+      </div>
       <button
         className="favorites__refresh"
         onClick={() => window.location.reload(false)}
       >
         Refresh Favorites
       </button>
+
       {props.databaseInfo.length || props.foodDatabase.length ? (
         <div className="container-fluid subContainer">
-          <div className=" movie__row">
-            <h1 className="favorite__header">Favorite Movies</h1>
+          <div className="row pt-5 ml-auto mr-auto ">
+            {/* <h1 className="favorite__header">
+              Movies <i class="fas fa-film"></i>
+            </h1> */}
+
             {props.databaseInfo.map(movie => (
               // Needed for differentiation
               <Fade key={uuidv4()}>
-                <div className="movie__SubRow fade-in2" key={movie._id}>
-                  <div className="movie__title">{movie.title}</div>
-                  <div className="movie__rating">Rating: {movie.rating}</div>
-                  <div className="movie__description">{movie.description}</div>
+                <div className=" fade-in2" key={movie._id}>
+                  <div className="card-body favorite-body card-color">
+                    <div className="favorite-card-title">
+                      <i className="fas fa-film"></i> {movie.title}{" "}
+                      {movie.rating} <i className=" star_icon fas fa-star"></i>
+                    </div>
+                    <p className="card-text favorite-text">
+                      {movie.description}
+                    </p>
+                  </div>
                 </div>
               </Fade>
             ))}
-          </div>
 
-          <div className=" restaurant__row ">
-            <h1 className="favorite__header">Favorite Restaurants</h1>
+            {/* <div className=" restaurant__row "> */}
+            {/* <h1 className="favorite__header">
+              Restaurants <i class="fas fa-utensils"></i>
+            </h1> */}
             {props.foodDatabase.map(food => (
               // Needed for differentiation
               <Fade key={uuidv4()}>
-                <div className="restaurant__SubRow fade-in2" key={food._id}>
-                  <div className="movie__title">{food.name}</div>
-                  <div className="movie__rating">Address: {food.address}</div>
+                <div className=" fade-in2" key={food._id}>
+                  <div className="card-body favorite-body card-color">
+                    <div className="favorite-card-title">
+                      <i className="fas fa-utensils"></i> {food.name}
+                    </div>
+                    <div className="card-text favorite-text">
+                      Address: {food.address}
+                    </div>
+                  </div>
                 </div>
               </Fade>
             ))}
           </div>
         </div>
       ) : (
-        <div className="favorites__placholder__container">
-          <div className="favorites__placeholder">
-            <p className="placeholder__paragraph">
+        <div className="favorites__placeholder__container container-flex">
+          <div className="container flex favorites__placeholder">
+            <p className="col-sm-6 col-m-6 col-lg-4 ml-auto mr-auto placeholder__paragraph">
               Hello! It seems like you don't have any favorites yet!
             </p>
           </div>
