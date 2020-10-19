f4b11d8fdc23774d796761d9148896facea17173
Cyrus Jose <cyrus.jose@outlook.com>
2020-10-18 19:56:31 -0700

    fixing build directory

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
-  app.use(express.static("build"));
+  app.use(express.static("../build"));
 
   // Express will serve up the front-end index.html file if it doesn't recognize the route
   app.get("*", (req, res) => res.sendFile(path.resolve("build", "index.html")));
