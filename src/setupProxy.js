89ec9d634aa8fc4aa3be059af0dc454f72ee7475
Cyrus Jose <cyrus.jose@outlook.com>
2020-10-18 19:22:38 -0700

    testing

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,6 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    proxy(["/login", "/signup", "/user", "/movie", "/restaurant", "/favorites" ], { target: "http://localhost:8080" })
+    proxy(["/user/login", "/user/signup", "/user/", "/user/logout", "/logout", "/movie", "/restaurant", "/favorites" ], { target: "http://localhost:8080" })
   );
 };
