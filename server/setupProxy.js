0250ecacc6d6c8f7d75bc9ef0b75c49b236f29db
Cyrus Jose <cyrus.jose@outlook.com>
2020-10-18 20:33:38 -0700

    Added proxy back

--- server/setupProxy.js
+++ server/setupProxy.js
@@ -1,7 +1,7 @@
-const proxy = require("http-proxy-middleware");
+// const proxy = require("http-proxy-middleware");
 
-module.exports = function(app) {
-  app.use(
-    proxy(["/user/login", "/user/signup", "/user/", "/user/logout", "/logout", "/movie", "/restaurant", "/favorites" ], { target: "http://localhost:8080" })
-  );
-};
+// module.exports = function(app) {
+//   app.use(
+//     proxy(["/user/login", "/user/signup", "/user/", "/user/logout", "/logout", "/movie", "/restaurant", "/favorites" ], { target: "http://localhost:8080" })
+//   );
+// };
